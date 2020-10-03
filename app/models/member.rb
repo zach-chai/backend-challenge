2d5bf354a4553e537fb28576def2b591f95d5b53
Zachary Chai <zachary.chai@outlook.com>
2020-10-03 16:37:43 -0400

    create friendships between members

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,8 @@
 class Member < ApplicationRecord
+  def friends
+    friendships = Friendship.where('member_id = ? OR friend_id = ?', id, id)
+    friend_ids = friendships.map(&:friend_id).concat(friendships.map(&:member_id))
+    friend_ids.delete(id)
+    Member.find(friend_ids)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_03_193412) do
+ActiveRecord::Schema.define(version: 2020_10_03_195751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friendships", force: :cascade do |t|
+    t.bigint "member_id", null: false
+    t.bigint "friend_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["member_id"], name: "index_friendships_on_member_id"
+  end
 
   create_table "members", force: :cascade do |t|
     t.string "name"
@@ -23,4 +32,6 @@ ActiveRecord::Schema.define(version: 2020_10_03_193412) do
     t.string "keywords", default: [], array: true
   end
 
+  add_foreign_key "friendships", "members"
+  add_foreign_key "friendships", "members", column: "friend_id"
 end
